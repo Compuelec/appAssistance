ad4db61234dc104a70db6554d2aa55926f1506ee
Paula Palma <paul.palma@duocuc.cl>
2023-08-26 11:32:20 -0400

    -Update: add directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { NavbarPageModule } from './modules/navbar/navbar.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterPageModule } from './modules/footer/footer.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), NavbarPageModule, AppRoutingModule, FooterPageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/modules/navbar/navbar.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { NavbarPageRoutingModule } from './navbar-routing.module';
+
+import { NavbarPage } from './navbar.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    NavbarPageRoutingModule
+  ],
+  declarations: [NavbarPage],
+  exports: [NavbarPage]
+})
+export class NavbarPageModule {}
