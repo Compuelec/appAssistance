a9f9aa028af6757ae8848b80750b8c124ecc088b
Carlos Abarca Vega <soporte@compuelec.cl>
2023-08-29 20:52:40 -0400

    -Update: add login structure

--- src/app/modules/auth/login/login-routing.module.ts
+++ src/app/modules/auth/login/login-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FooterPage } from './footer.page';
+import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: FooterPage
+    component: LoginPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FooterPageRoutingModule {}
+export class LoginPageRoutingModule {}
