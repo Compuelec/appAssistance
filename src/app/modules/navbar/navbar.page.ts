b96645cc8ae93526072ec55e6a3b0396e3d380c1
Carlos Abarca Vega <soporte@compuelec.cl>
2023-09-02 08:58:13 -0400

    -Update: add session start ok and logout

--- src/app/modules/navbar/navbar.page.ts
+++ src/app/modules/navbar/navbar.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.page.scss'],
 })
 export class NavbarPage implements OnInit {
+  studentLogged: string = '';
+  studentEmail: string = '';
+  studentRole: string = '';
 
-  constructor() { }
+  logout = false;
+
+  token = localStorage.getItem('token');
+
+  constructor(private _cdr: ChangeDetectorRef, private router: Router) { }
 
   ngOnInit() {
+
+    if (this.token !== null) {
+      const decoded: any = jwt_decode(this.token);
+      console.log('Token decodificado:', decoded);
+      this.studentLogged = decoded['username'];
+      this.studentEmail = decoded['email'];
+      this.studentRole = decoded['role'];
+      this.logout = true;
+    }
+    this._cdr.detectChanges();
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token');
+    this.logout = false;
+    this.router.navigate(['/login']);
   }
 
 }
