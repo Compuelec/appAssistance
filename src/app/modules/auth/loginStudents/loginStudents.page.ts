783b6030d952e814c868110ccb065b0d00f8e958
Carlos Abarca Vega <soporte@compuelec.cl>
2023-09-02 10:44:59 -0400

    update: message error in login

--- src/app/modules/auth/loginStudents/loginStudents.page.ts
+++ src/app/modules/auth/loginStudents/loginStudents.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+
 import { AuthService } from '../../../services/apiLogin.service';
 import { LoginData } from '../../../interfaces/login.interface';
 
@@ -11,8 +14,11 @@ import { LoginData } from '../../../interfaces/login.interface';
 export class LoginStudentsPage implements OnInit {
   email:string = "";
   password:string = "";
+  messageError:string = "";
 
-  constructor(private _authService: AuthService, private router: Router) { }
+  private subscription: Subscription | undefined;
+
+  constructor(private _authService: AuthService, private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
     const token = localStorage.getItem('token');
@@ -22,24 +28,52 @@ export class LoginStudentsPage implements OnInit {
       return;
     }
   }
-
-login(email: string, password: string) {
-  const credentials: LoginData = { email, password };
-  
-  this._authService.loginStudents(credentials).subscribe(
-    (response: any) => {
-      if (response && response.token) {
-        localStorage.setItem('token', response.token); // Almacena el token en el localStorage
-        this.router.navigate(['/home']);
-      } else {
-        console.log("Inicio de sesión fallido");
+  login(email: string, password: string) {
+    if (!this.isValidEmail(email) || !this.isValidPassword(password)) {
+        this.mostrarErrorAlert('Email no valido o contraseña no valida');
+        return;
       }
-    },
-    (error: any) => {
-      console.error("Error al iniciar sesión: ", error);
+    const credentials: LoginData = { email, password };
+
+    this.subscription = this._authService.loginStudents(credentials).subscribe({
+      next: (response: any) => {
+        if (response && response.token) {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/home']);
+        }
+      },
+      error: (error: any) => {
+        if (error.status === 401) {
+          this.mostrarErrorAlert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
+        }
+      },
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
-  );
-}
+  }
+
+  async mostrarErrorAlert(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Error de Credenciales',
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  isValidEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  }
+
+  isValidPassword(password: string): boolean {
+    return password.length >= 6;
+  }
 
 
 }
--- src/app/modules/navbar/navbar.page.ts
+++ src/app/modules/navbar/navbar.page.ts
@@ -12,8 +12,6 @@ export class NavbarPage implements OnInit {
   studentEmail: string = '';
   studentRole: string = '';
 
-  logout = false;
-
   token = localStorage.getItem('token');
 
   constructor(private _cdr: ChangeDetectorRef, private router: Router) { }
@@ -22,18 +20,15 @@ export class NavbarPage implements OnInit {
 
     if (this.token !== null) {
       const decoded: any = jwt_decode(this.token);
-      console.log('Token decodificado:', decoded);
       this.studentLogged = decoded['username'];
       this.studentEmail = decoded['email'];
       this.studentRole = decoded['role'];
-      this.logout = true;
     }
     this._cdr.detectChanges();
   }
 
   logoutUser() {
     localStorage.removeItem('token');
-    this.logout = false;
     this.router.navigate(['/login']);
   }
 
