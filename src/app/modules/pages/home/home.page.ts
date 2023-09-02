940352fe10b2f4d7564c02d7529fc153bef20ef7
Carlos Abarca <soporte@compuelec.cl>
2023-09-02 02:09:52 -0400

    -Update: Add jwt decode

--- src/app/modules/pages/home/home.page.ts
+++ src/app/modules/pages/home/home.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  studentLogged: string = '';
+  studentEmail: string = '';
+  studentRole: string = '';
 
   constructor() {}
+
+  ngOnInit() {
+
+  const token = localStorage.getItem('token');
+
+  if (token !== null) {
+    const decoded: any = jwt_decode(token);
+    console.log('Token decodificado:', decoded);
+    this.studentLogged = decoded['username'];
+    this.studentEmail = decoded['email'];
+    this.studentRole = decoded['role'];
+  } else {
+    console.log("No hay token");
+  }
+
+}
 
 }
